df7159e67c501ce0e3de64b2185880bd0cc1e716
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-05-09 23:56:46 -0400

    fixed key error in connection

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config()
+console.log(process.env);
 
 const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: process.env.MYSQL_PASS,
+        password: process.env.SECRET_KEY,
         database: 'employee'
 
     }
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
-
 const db = require('./db/connection');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+})
